932738a8ab48f7fb93d228139d8128ccff3408a9
Raphael Balogo <balogoraphael@gmail.com>
2020-08-18 00:17:32 -0300

    remove node_modules

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.post();
+// @route       POST api/users
+// @desc        Register a user
+// @access      Public
+router.post('/', (req, res) => {
+	res.send('Register a user');
+});
+
+module.exports = router;
